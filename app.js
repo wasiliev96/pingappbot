da5b4ea323b577b45b42ebf69ce6074ff9c5490f
wasiliev96 <wasiliev.cloud@gmail.com>
2020-08-27 17:03:32 +0300

    Works?

--- app.js
+++ app.js
@@ -8,14 +8,13 @@ let data = JSON.parse(fs.readFileSync("data.json"));
 console.log(data);
 // Create a bot that uses 'polling' to fetch new updates
 const bot = new TelegramBot(token, { polling: true });
-let pingtime = 1000;
-
+let pingtime = 2000;
+let intervals = [];
 // Matches "/echo [whatever]"
 bot.onText(/\/addsite (.+)/, (msg, match) => {
   // 'msg' is the received Message from Telegram
   // 'match' is the result of executing the regexp above on the text content
   // of the message
-
   const chatId = msg.chat.id;
   const resp = match[1]; // the captured "whatever"
   const obj = {
@@ -27,6 +26,7 @@ bot.onText(/\/addsite (.+)/, (msg, match) => {
   fs.writeFileSync("data.json", JSON.stringify(data));
   console.log(data);
   // send back the matched "whatever" to the chat
+  pingSites(data.sites, chatId);
   bot.sendMessage(
     chatId,
     `Сайт ${resp} записан в базу данных. Опрос будет производиться один раз в ${
@@ -48,7 +48,15 @@ bot.onText(/\/time (.+)/, (msg, match) => {
     `Частота опроса теперь - 1 раз в ${resp / 1000} секунд`
   );
 });
-bot.onText(/\/show/, (msg) => {
+bot.onText(/\/stop/, (msg) => {
+  intervals.map((interval) => {
+    // console.log(interval);
+    clearInterval(interval);
+    arr = [];
+  });
+  bot.sendMessage(msg.chat.id, "stopping...");
+});
+bot.onText(/\/list/, (msg) => {
   const chatId = msg.chat.id;
   data.sites.forEach((site) => {
     console.log(typeof site);
@@ -64,9 +72,27 @@ bot.onText(/\/remove (.+)/, (msg, match) => {
   console.log(typeof resp);
   // arr = arr.filter(e => e !== 'B');
   fs.writeFileSync("data.json", JSON.stringify(data));
+  pingSites(data.sites, chatId);
   bot.sendMessage(chatId, `Сайт ${resp} удален из базы`);
 });
-
+bot.onText(/\/run/, (msg) => {
+  const chatId = msg.chat.id;
+  bot.sendMessage(chatId, `Running...`);
+  data.sites.forEach(function (host) {
+    console.log(host);
+    intervals.push(
+      setInterval(function () {
+        ping.sys.probe(host, function (active) {
+          var info = active
+            ? "IP " + host + " = Active"
+            : "IP " + host + " = Non-Active";
+          console.log(info);
+          !active && bot.sendMessage(chatId, `${host} is down!`);
+        });
+      }, pingtime)
+    );
+  });
+});
 // Listen for any kind of message. There are different kinds of
 // messages.
 // bot.on('message', (msg) => {
@@ -75,3 +101,4 @@ bot.onText(/\/remove (.+)/, (msg, match) => {
 //   // send a message to the chat acknowledging receipt of their message
 //   bot.sendMessage(chatId, 'Сайт принят');
 // });
+function pingSite(host, chatId) {}
